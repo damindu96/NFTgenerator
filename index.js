f20536ccd934cd080ab974516ca8d4f00bf778a4
damindu96 <daminduwelikala96@gmail.com>
2022-05-23 22:34:39 +0530

    4th v

--- index.js
+++ index.js
@@ -1,19 +1,55 @@
 const fs = require("fs");
+const myArgs = process.argv.slice(2);
 const { createCanvas, loadImage } = require("canvas");
-const console = require("console");
-const canvas =  createCanvas(1000,1000);
-const ctx = canvas.getContext("2d");
 const {layers, width, height} = require("./input/config.js");
-const edition = 1;
+const console = require("console");
+const { has } = require("cheerio/lib/api/traversing");
+const canvas =  createCanvas(width, height);
+const ctx = canvas.getContext("2d");
+const edition = myArgs.length > 0 ? Number(myArgs[0]) : 1;
+var metadata = [];
+var attributes = [];
+var hash = [];
+var decodedHash = [];
+
+
 
 const saveLayer = (_canvas, _edition) => {
     fs.writeFileSync(`./output/${_edition}.png`, _canvas.toBuffer("image/png"));
     //console.log("Image created");
 }
 
+const addMetadata = (_edition) => {
+    let dateTime = Date.now();
+    let tempMetadata = {
+        hash : hash.join(""),
+        decodedHash : decodedHash,
+        edition : _edition,
+        date : dateTime,
+        attributes : attributes,
+    };
+    metadata.push(tempMetadata);
+    attributes = [];
+    hash = [];
+    decodedHash = [] ; 
+};
+
+const addAttributes = (_element, _layer) =>{
+    let tempAttr = {
+        id : _element.id,
+        layer : _layer.name,
+        name : _element.name,
+        rarity : _element.rarity
+    }
+    attributes.push(tempAttr);
+    hash.push(_layer.id);
+    hash.push(_element.id),
+    decodedHash.push({[_layer.id]: _element.id})
+};
+
 const drawLayer = async (_layer, _edition) => {
     let element = _layer.elements[Math.floor(Math.random() * _layer.elements.length)];
-   // console.log(element);
+    addMetadata(element, _layer);
     const image = await loadImage(`${_layer.location}${element.fileName}`);
     ctx.drawImage(
       image, 
@@ -22,8 +58,7 @@ const drawLayer = async (_layer, _edition) => {
         _layer.size.width, 
         _layer.size.height
         );
-    console.log(`I created the ${_layer.name} layer, and choose element ${element.name}`
-    );
+
    saveLayer(canvas, _edition);
 };
 
@@ -31,5 +66,11 @@ for(let i = 1; i <= edition; i++){
     layers.forEach(layer => {
         drawLayer(layer, i);
     });
+    addMetadata(i);
     console.log("creating edition" + i);
 }
+
+fs.readFile("./output/_metadata.json", (err, data) => {
+    if(err) throw err;
+    fs.writeFileSync("./output/_metadata.json", JSON.stringify(metadata));
+})
